1e81b27218ff7c4330f5a5dd99ac005b015c4e90
Max Jenslöv <max@jenslov.se>
2026-04-21 11:10:15 +0200

    Init. Commit

---
+++ Invoke-DevOps.ps1
@@ -0,0 +1,80 @@
+﻿# Group
+#     az devops : Manage Azure DevOps organization level operations.
+function Invoke-DevOps {az devops @args}
+
+#         Related Groups
+#         az pipelines: Manage Azure Pipelines
+#         az boards: Manage Azure Boards
+#         az repos: Manage Azure Repos
+#         az artifacts: Manage Azure Artifacts.
+
+# Subgroups:
+#     admin            : Manage administration operations.
+#     extension        : Manage extensions.
+#     project          : Manage team projects.
+#     security         : Manage security related operations.
+#     service-endpoint : Manage service endpoints/connections.
+#     team             : Manage teams.
+#     user             : Manage users.
+#     wiki             : Manage wikis.
+
+# Commands:
+#     configure        : Configure the Azure DevOps CLI or view your configuration.
+#     invoke           : This command will invoke request for any DevOps area and resource. Please use
+#                        only json output as the response of this command is not fixed. Helpful docs -
+#                        https://docs.microsoft.com/en-us/rest/api/azure/devops/.
+#     login            : Set the credential (PAT) to use for a particular organization.
+#     logout           : Clear the credential for all or a particular organization.
+
+#region az boards
+
+# Group
+#     az boards : Manage Azure Boards.
+#         This command group is a part of the azure-devops extension.
+function Invoke-DevOpsBoards {az boards @args}
+
+# Commands:
+#     query     : Query for a list of work items.
+function Get-DevOpsBoards {Invoke-DevOpsBoards query @args }
+
+# Subgroups:
+#     area      : Manage area paths.
+#     iteration : Manage iterations.
+#     work-item : Manage work items.
+
+#region az boards work-item
+
+# Group
+#     az boards work-item : Manage work items.
+function Invoke-DevOpsWorkItems {Invoke-DevOpsBoards work-item @args}
+
+# Commands:
+#     create   : Create a work item.
+#     delete   : Delete a work item.
+#     show     : Show details for a work item.
+#     update   : Update work items.
+function New-WorkItem {Invoke-DevOpsWorkItems create @args}
+function Remove-WorkItem {Invoke-DevOpsWorkItems remove @args}
+function Show-WorkItem {Invoke-DevOpsWorkItems show @args}
+function Update-WorkItem {Invoke-DevOpsWorkItems update @args}
+
+# Subgroups:
+#     relation : Manage work item relations.
+
+#region az boards work-item relation
+# Group
+#     az boards work-item relation : Manage work item relations.
+function Invoke-DevOpsRelations {Invoke-DevOpsWorkItems relation @args}
+
+# Commands:
+#     add       : Add relation(s) to work item.
+#     list-type : List work item relations supported in the organization.
+#     remove    : Remove relation(s) from work item.
+#     show      : Get work item, fill relations with friendly name.
+function Add-WorkItemRelations {Invoke-DevOpsRelations add @args}
+function Get-WorkItemRelations {Invoke-DevOpsRelations list-type @args}
+function Remove-WorkItemRelations {Invoke-DevOpsRelations remove @args}
+function Show-WorkItemRelations {Invoke-DevOpsRelations show @args}
+#endregion az boards work-item relation
+#endregion az boards work-item
+#endregion az boards
---
+++ New-AzureWorkItem.ps1
@@ -0,0 +1,96 @@
+﻿function New-AzureWorkItem {
+    [CmdletBinding()]
+    param (
+        # Required arguments
+        [Parameter(Mandatory=$true)]
+        [string]$Title,
+
+        [ValidateSet('Bug', 'User Story')]
+        [string]$Type = 'User Story',
+
+        # Optional arguments
+        [string]$Area,
+
+        [Alias('AssignedTo')]
+        [string]$Assigned_To,
+
+        [Alias('d')]
+        [string]$Description,
+
+        [ValidateSet('true', 'false')]
+        [string]$Detect,
+
+        [string]$Discussion,
+
+        [Alias('f')]
+        [string[]]$Fields,
+
+        [string]$Iteration,
+
+        [switch]$Open,
+
+        [Alias('Organization')]
+        [string]$Org,
+
+        [Alias('p')]
+        [string]$Project,
+
+        [string]$Reason,
+
+        # Global arguments
+        [Alias('o')]
+        [ValidateSet('json', 'jsonc', 'none', 'table', 'tsv', 'yaml', 'yamlc')]
+        [string]$Output,
+
+        [Alias('h')]
+        [switch]$Help,
+
+        [string]$Query
+    )
+
+
+
+    # Base az command
+    $args = @(
+        '--title', $Title,
+        '--type',  $Type
+    )
+
+    # Helper to conditionally add parameters
+    function Add-Arg {
+        param ($Name, $Value)
+
+        if ($null -ne $Value -and $Value -ne '') {
+            $args += @($Name, $Value)
+        }
+    }
+
+    Add-Arg '--area'        $Area
+    Add-Arg '--assigned-to' $Assigned_To
+    Add-Arg '--description' $Description
+    Add-Arg '--detect'       $Detect
+    Add-Arg '--discussion'   $Discussion
+    Add-Arg '--iteration'    $Iteration
+    Add-Arg '--org'          $Org
+    Add-Arg '--project'      $Project
+    Add-Arg '--reason'       $Reason
+    Add-Arg '--output'       $Output
+    Add-Arg '--query'        $Query
+
+    if ($Fields) {
+        foreach ($field in $Fields) {
+            $args += @('--fields', $field)
+        }
+    }
+
+    if ($Open.IsPresent) {
+        $args += '--open'
+    }
+
+    if ($Help) {
+        $args += '--help'
+    }
+
+    # Execute the az CLI command
+    az boards work-item create @args
+}
